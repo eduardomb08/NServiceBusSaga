5032b24b171b2b858b9c3b176d1f6d84180b4a81
Eduardo Monteiro de Barros <eduardo.mb@amwins.com>
2019-03-08 09:39:45 -0500

    Add project files.

---
+++ NServiceBusSagaService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NServiceBusSagaService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
